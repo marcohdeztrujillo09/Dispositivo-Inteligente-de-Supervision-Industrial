0a00f04f55a787592e0bc5d9ba57fee58265eb03
Marco Antonio Hernández Trujillo <marcohdeztrujillo09@gmail.com>
2026-06-16 16:28:13 +0100

    Delete dispositivo-iot-planta-inteligente directory

--- dispositivo-iot-planta-inteligente/esp32/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-// ----------- CONFIGURACIÓN WIFI Y MQTT (SECRETO) -----------
-const char* ssid = "TU_WIFI_SSID";
-const char* password = "TU_CONTRASEÑA_WIFI";
-const char* mqtt_server = "192.168.X.XXX"; // IP de tu bróker MQTT
-
-#endif
