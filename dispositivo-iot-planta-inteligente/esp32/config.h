9c35f1d97859b92384faff1091613f418613086d
Marco Antonio Hernández Trujillo <marcohdeztrujillo09@gmail.com>
2026-06-16 16:24:06 +0100

    Add files via upload

---
+++ dispositivo-iot-planta-inteligente/esp32/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// ----------- CONFIGURACIÓN WIFI Y MQTT (SECRETO) -----------
+const char* ssid = "devolo-019";
+const char* password = "HFKPTTODQMYFJZVC";
+const char* mqtt_server = "192.168.1.23"; // IP de tu PC local
+
+#endif
